27665f1cad3fa6ff5db01da5ea6e87fba1b5c47c
T357A <T357A@users.noreply.github.com>
2022-01-25 22:09:00 -0500

    added libraries

---
+++ Sensors.py
@@ -0,0 +1,16 @@
+# Different types of sensor types for future pluggins.
+# Light detection
+# Camera(s)- motion, identification, collision avoidance and tracking
+# Ir Camera (Night vision)
+# Elevation
+# Temperature
+# Geolocation
+# Stepper motors for autonomy (pan/ tilt) and motion (drone or tank)
+# wireless controllers(?)
+# Bluetooth
+# wifi dongle
+# battery monitor- through exsiting hardware? How to notify- light/ beeping text/ email?
+# Cellular/ sim for texting/ updating (twilio)
+# Capture/ deactivation switch
+# Directional microphones
+# Speaker- sound effects etc
